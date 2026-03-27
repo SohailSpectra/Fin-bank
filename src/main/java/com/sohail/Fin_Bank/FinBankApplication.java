4e7dafd5fc5d78bfd8b766ad2753130f0b80eebc
Soohail94 <posttosohail@gmail.com>
2026-03-27 12:36:14 +0530

    connected to database

---
+++ src/main/java/com/sohail/Fin_Bank/FinBankApplication.java
@@ -0,0 +1,13 @@
+package com.sohail.Fin_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sohail/Fin_Bank/FinBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohail.Fin_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
